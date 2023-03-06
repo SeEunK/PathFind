e894fa7d8201649e26880afb382126f0a70412e2
KSE <silverami.k@gmail.com>
2023-03-06 13:20:04 +0900

    add MapController

--- PathFind/Assets/01.UnityProject/Scripts/Global/GFunc+Develop.cs
+++ PathFind/Assets/01.UnityProject/Scripts/Global/GFunc+Develop.cs
@@ -49,7 +49,33 @@ public static partial class GFunc
     #endregion      // Assert for debug
 
     #region Vaild Func
-    public static bool IsValid<T>(this T component_)
+
+    // 오브젝트의 유효성을 검사하는 함수
+    public static bool IsValid(this GameObject obj_)
+    {
+        bool isInValid = (obj_ == null || obj_ == default);
+        return !isInValid;
+    }
+
+
+    //리스트의 유효성을 검사.
+    public static bool IsValid<T>(this List<T> list_)
+    {
+        bool isInValid = (list_ == null || list_ == default || list_.Count <1);
+        return !isInValid;
+    }
+
+    //리스트의 유효성을 검사.
+    public static bool IsValid<T>(this List<T> list_, int index_)
+    {
+        bool isInValid = (list_.IsValid() == false || index_ < 0 || list_.Count <= index_);
+        return !isInValid;
+    }
+
+
+
+    // 컴포넌트의 유효성을 검사하는 함수
+    public static bool IsValid<T>(this T component_) where T : Component
     {
         Component convert_ = (Component)(component_ as Component);
         bool isInvalid = convert_ == null || convert_ == default;
